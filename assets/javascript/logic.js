6878d1dcd92a6ed6fa60fcfcfeb10ba6334546a2
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-07-14 17:41:03 -0400

    code update 3

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,7 +2,9 @@ $(document).ready(function()    {
 
     $("#name-header").animate({opacity: "1", marginTop: "+=200px"}, 3000);
 
-    $(".footer").animate({opacity: "1", marginTop: "-=75px"}, 4000);
+
+    $("a").animate({opacity: "1", marginTop: "-=75px"}, 4000);
+
 
     $("#about-header").animate({opacity: "1"}, 2000);
 
@@ -10,8 +12,11 @@ $(document).ready(function()    {
 
     $(".auth-image").animate({opacity: "1"}, 2000);
 
-    
+    $(".work").animate({opacity: "1"}, 2000);
 
+    $("h3").animate({opacity: "1"}, 2000);
+
+    $("img").animate({opacity: "1"}, 2000);
 
 });
 
