d89b02f71dcf58963b205c450326d844b4611af8
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-07-14 23:32:43 -0400

    final commit 0714

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,23 +1,14 @@
 $(document).ready(function()    {
-
     $("#name-header").animate({opacity: "1", marginTop: "+=200px"}, 3000);
-
-
-    $("a").animate({opacity: "1", marginTop: "-=75px"}, 4000);
-
-
-    $("#about-header").animate({opacity: "1"}, 2000);
-
+    $("a").animate({opacity: "1"}, 4000);
+    $("#page-headers").animate({opacity: "1"}, 2000);
     $("p").animate({opacity: "1"}, 2000);
-
     $(".auth-image").animate({opacity: "1"}, 2000);
-
     $(".work").animate({opacity: "1"}, 2000);
-
     $("h3").animate({opacity: "1"}, 2000);
-
+    $(".connect").animate({opacity: "1"}, 2000);
     $("img").animate({opacity: "1"}, 2000);
-
+    $("#contact-form").animate({opacity: "1"}, 2000);
 });
 
   
